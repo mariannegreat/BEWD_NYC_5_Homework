a76dfb0bb6bd9cdc0a00fed6a0a06a810f392b9f
mariannegreat <mariannepdo@gmail.com>
2013-11-10 21:09:51 -0500

    Midterm project

---
+++ marianne/midterm/spotify.rb
@@ -0,0 +1,17 @@
+class Spotify
+    def initialize tracks
+    	@album = tracks["album"]["name"]
+        @released = tracks["album"]["released"]
+        @song_name = tracks["name"]
+        @artist_name = tracks["artists"][0]["name"]
+    end
+
+    def print
+        puts "Song: #{@song_name}"
+        puts "Artist: #{@artist_name}"
+    	puts "Album: #{@album} (#{@released})"
+
+        puts @artists
+        puts "\n"
+    end
+end
